e46ede3ea4d6cf335d56572ec72f7fbd88f1e8fd
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-11 22:24:27 +0300

    Create task033.js

---
+++ js/task033.js
@@ -0,0 +1,21 @@
+/*Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000.
+
+Examples (input --> output)
+4 --> 3 (1, 2, 4)
+5 --> 2 (1, 5)
+12 --> 6 (1, 2, 3, 4, 6, 12)
+30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+*/
+
+function getDivisorsCnt(n) {
+  // todo
+}
+
+console.log(getDivisorsCnt(1)) // 1
+console.log(getDivisorsCnt(10)) // 4
+console.log(getDivisorsCnt(11)) // 2
+console.log(getDivisorsCnt(54)) // 8
