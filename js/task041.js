c4fb0009728937470ac03d663d44048a457e75cf
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-14 21:52:53 +0300

    array.map()

---
+++ js/task041.js
@@ -0,0 +1,17 @@
+/*Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]*/
+
+const number = function (array) {
+  return array.map((line, index) => `${index + 1}: ${line}`)
+}
+
+console.log(number([])) //--> []
+console.log(number(['a', 'b', 'c'])) //--> ["1: a", "2: b", "3: c"]
