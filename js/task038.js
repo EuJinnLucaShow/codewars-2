57ba44dae04acd11c3782b0d5d265b43751a9bf6
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-14 20:59:37 +0300

    Update task038.js

--- js/task038.js
+++ js/task038.js
@@ -1,6 +1,24 @@
-let arr = [1, 2, 3]
-let arr2 = [4, 5]
+/*Write function bmi that calculates body mass index (bmi = weight / height2).
 
-arr = [...arr, ...arr2]
+if bmi <= 18.5 return "Underweight"
 
-console.log(arr) // [ 1, 2, 3, 4, 5 ]
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"*/
+
+function bmi(weight, height) {
+  const bmiCalc = weight / Math.pow(height, 2)
+  if (bmiCalc <= 18.5) {
+    return 'Underweight'
+  } else if (bmiCalc <= 25.0) {
+    return 'Normal'
+  } else if (bmiCalc <= 30.0) {
+    return 'Overweight'
+  } else if (bmiCalc > 30) {
+    return 'Obese'
+  }
+}
+
+console.log(bmi(80, 1.8)) // 'Normal'
