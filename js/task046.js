7b200ee11f327cf1fedf0f6eda8a3085ae6e0368
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-15 14:03:27 +0300

    Math.max(...list)

---
+++ js/task046.js
@@ -0,0 +1,12 @@
+var min = function (list) {
+  return Math.min(...list)
+}
+
+var max = function (list) {
+  return Math.max(...list)
+}
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110])) // -110
+console.log(min([42, 54, 65, 87, 0])) // 0
+console.log(max([4, 6, 2, 1, 9, 63, -134, 566])) // 566
+console.log(max([5])) // 5
