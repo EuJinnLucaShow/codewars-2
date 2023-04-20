d92a5a01956f84758d10d55ba384b56cd8d3698c
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-20 12:46:53 +0300

    Summary

--- js/task048.js
+++ js/task048.js
@@ -6,24 +6,28 @@ uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]*/
 
+// function uniqueInOrder(iterable) {
+//   // If the iterable is empty, return an empty array
+//   if (!iterable.length) {
+//     return []
+//   }
+
+//   // Initialize the result array with the first element of the iterable
+//   let result = [iterable[0]]
+
+//   // Iterate over the remaining elements of the iterable
+//   for (let i = 1; i < iterable.length; i++) {
+//     // If the current element is not the same as the previous element, add it to the result array
+//     if (iterable[i] !== iterable[i - 1]) {
+//       result.push(iterable[i])
+//     }
+//   }
+
+//   return result
+// }
+
 function uniqueInOrder(iterable) {
-  // If the iterable is empty, return an empty array
-  if (!iterable.length) {
-    return []
-  }
-
-  // Initialize the result array with the first element of the iterable
-  let result = [iterable[0]]
-
-  // Iterate over the remaining elements of the iterable
-  for (let i = 1; i < iterable.length; i++) {
-    // If the current element is not the same as the previous element, add it to the result array
-    if (iterable[i] !== iterable[i - 1]) {
-      result.push(iterable[i])
-    }
-  }
-
-  return result
+  return [...iterable].filter((a, i) => a !== iterable[i - 1])
 }
 
 console.log(uniqueInOrder('AAAABBBCCDAABBB')) // ['A', 'B', 'C', 'D', 'A', 'B']
