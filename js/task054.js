fc2184320b8f6e076b8724bac5010d7113247194
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-21 19:39:58 +0300

    Update task054.js

--- js/task054.js
+++ js/task054.js
@@ -1,26 +1,20 @@
-// function incrementString(strng) {
-//   let letter = strng.slice(strng.length - 1, strng.length)
-//   if (Number(letter) === NaN) {
-//     return strng + 1
-//   } else {
-//     letter = Number(letter) + 1
-//   }
-//   return String(letter)
-// }
-
 function incrementString(strng) {
-  const match = strng.match(/\d*$/)
-  if (match[0]) {
-    let numStr = match[0]
-    if (numStr) {
-      let num = parseInt(numStr, 10)
-      num += 1
-      numStr = num.toString().padStart(numStr.length, '0')
-      return strng.slice(0, -numStr.length) + numStr
-    }
-  }
-  return strng + '1'
+  return strng.replace(/([0-8]|\d?9+)?$/, e => (e ? +e + 1 : 1))
 }
+
+// function incrementString(strng) {
+//   const match = strng.match(/\d*$/)
+//   if (match[0]) {
+//     let numStr = match[0]
+//     if (numStr) {
+//       let num = parseInt(numStr, 10)
+//       num += 1
+//       numStr = num.toString().padStart(numStr.length, '0')
+//       return strng.slice(0, -numStr.length) + numStr
+//     }
+//   }
+//   return strng + '1'
+// }
 
 console.log(incrementString('foo')) // -> foo1
 console.log(incrementString('foobar23')) // -> foobar24
